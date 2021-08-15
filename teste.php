64bf3c269ea6a64b29f18bac7bf2c871c0e04896
CapuT-T <Rodrigo@gmail.com>
2021-08-15 14:24:58 -0500

    Segunda questão

--- teste.php
+++ teste.php
@@ -1,19 +1,40 @@
 <?php
+$valorInicial = 10;
+$valorFinal = 50;
 
-$a = 'código;php;linguagem*nota;ifac;aluno*professor;bimestre;prova*computador;programação;web';
-$linhas = explode('*', $a);
+$valorVoltas = ($valorFinal - $valorInicial) / 5;
+
+$ultimoNumero = $valorInicial;
 
 ?>
+
+<style>
+    .par {
+        color: #5e97f2;
+    }
+
+    .impar {
+        color: #5ef26a;
+    }
+</style>
 
 <table>
     <?php
     
-    foreach ($linhas as $linha) {
-        $colunas = explode(';', $linha);
-    
+    for ($i = 0; $i < $valorVoltas; $i++) {
         echo '<tr>';
-        foreach ($colunas as $coluna)
-            echo '<td>'.$coluna.'</td>';
+        for ($j = 0; $j < 5; $j++) {
+            if ($ultimoNumero + 1 < $valorFinal) {
+                $ultimoNumero = $ultimoNumero + 1;
+                if ($ultimoNumero % 2 == 0)
+                    echo '<td class="par">'.$ultimoNumero.'</td>';  
+                else
+                    echo '<td class="impar">'.$ultimoNumero.'</td>';
+            } else {
+                echo '<td>---</td>';
+            }
+        }
+    
         echo '</tr>';
     }
 
