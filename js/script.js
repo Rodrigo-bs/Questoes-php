9b29f615a9ad146b762ebfc3ef64b6634c95b74d
CapuT-T <Rodrigo@gmail.com>
2021-08-17 14:25:21 -0500

    Botão de copiar

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 const buttons = document.querySelectorAll('.button-copy');
 
 function copiarText(text) {
-  let textarea = document.createElement('textarea');
-  textarea.value = text;
-  document.body.appendChild(textarea);
+  let textarea = text.querySelector('textarea');
   textarea.focus();
   textarea.select();
 
   let command = document.execCommand('copy');
 
-  document.body.removeChild(textarea);
   if (command) return 1;
   else return 0;
 }
 
-function pegarValor(event) {
-  let valor = event.target.getAttribute('label-copy');
-  copiarText(valor);
+function pegarValor({ currentTarget }) {
+  let text = currentTarget.parentNode;
+  let status = copiarText(text);
+  console.log(status);
 }
 
 buttons.forEach(button => {
